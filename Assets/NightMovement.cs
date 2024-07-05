648d77fc3e503092bbb832e40ce906c6aef36b05
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2024-07-05 13:30:15 +0300

    player movement with animations (temporary models)

--- Assets/NightMovement.cs
+++ Assets/NightMovement.cs
@@ -9,6 +9,7 @@ public class NightMovement : NetworkBehaviour
     [Header("Character Movement")]
     [SerializeField] private CharacterController characterController = null;
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
     float gravity_ = -9.81f;
 
     [Header("Ground Check")]
@@ -16,24 +17,49 @@ public class NightMovement : NetworkBehaviour
     [SerializeField] private LayerMask groundMask = new LayerMask();
     float checkRadius = 0.4f;
 
+    [Header("Animatoions")]
+    [SerializeField] private Animator animator;
+
     Vector3 velocity;
 
     private void Update()
     {
-        if(!isLocalPlayer) return;
+        if (!isLocalPlayer) return;
 
-        bool isGrounded = check
         PlayerInput();
     }
 
     private void PlayerInput()
     {
+        bool isGrounded = Physics.CheckSphere(feet.transform.position, checkRadius, groundMask);
+
+        if (isGrounded && velocity.y <= 0)
+        {
+            velocity.y = -2f;
+        }
+
         float X = Input.GetAxis("Horizontal");
         float Z = Input.GetAxis("Vertical");
+
+        if (X != 0 || Z != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
 
         Vector3 move_ = transform.right * X + transform.forward * Z;
 
         characterController.Move(move_ * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isGrounded) return;
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity_);
+        }
 
         velocity.y += gravity_ * Time.deltaTime;
 
